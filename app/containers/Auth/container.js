8b9d7f614514e319460d70cba428f835b5524ff8
Davy Vong <davy.vong@intelex.com>
2018-04-27 11:31:07 -0400

    Hide discord token when logged in

--- app/containers/Auth/container.js
+++ app/containers/Auth/container.js
@@ -11,52 +11,54 @@ export default class Container extends React.PureComponent {
       isLoggedIn: false,
     };
   }
-  authDiscord = (event) => {
+  loginDiscord = (event) => {
+    event.preventDefault();
+    if (!this.state.isLoggedIn) {
+      this.props.loginDiscord({ token: this.props.token });
+      this.setState({ isLoggedIn: true });
+    }
+  }
+  logoutDiscord = (event) => {
     event.preventDefault();
     if (this.state.isLoggedIn && this.props.isLoggedIn) {
       this.props.logoutDiscord();
       this.setState({ isLoggedIn: false });
-    } else {
-      this.props.loginDiscord({ token: this.props.token });
-      this.setState({ isLoggedIn: true });
     }
   }
   updateToken = (event) => {
     event.preventDefault();
     this.props.updateToken({ token: event.target.value });
   }
-  /* eslint-disable jsx-a11y/label-has-for */
-  renderForm = (isLoggedIn) => (
-    <div>
-      <Field>
-        <label className="label">Discord Token</label>
-        <div className="control">
-          <input
-            className="input"
-            disabled={isLoggedIn}
-            onChange={this.updateToken}
-            placeholder="Discord Token"
-            value={this.props.token}
-          />
-        </div>
-      </Field>
-      <button className={`button is-fullwidth ${isLoggedIn ? 'is-danger' : 'is-info'}`} onClick={this.authDiscord}>
-        {isLoggedIn ? 'Logout' : 'Login'}
-      </button>
-    </div>
-  )
-  /* eslint-enable jsx-a11y/label-has-for */
   render() {
     const isLoggedIn = this.state.isLoggedIn && this.props.isLoggedIn;
+    /* eslint-disable jsx-a11y/label-has-for */
     return (
       <div>
-        {isLoggedIn ? this.renderForm(isLoggedIn) : (
-          <Modal isActive={!isLoggedIn}>
-            {this.renderForm(isLoggedIn)}
-          </Modal>
-        )}
+        <Field>
+          <label className="label">Discord Token</label>
+          <div className="control">
+            <input className="input" disabled value="secret" />
+          </div>
+        </Field>
+        <button className="button is-fullwidth is-danger" onClick={this.logoutDiscord}>Logout</button>
+        <Modal isActive={!isLoggedIn}>
+          <Field>
+            <label className="label">Discord Token</label>
+            <div className="control">
+              <input
+                className="input"
+                disabled={isLoggedIn}
+                onChange={this.updateToken}
+                placeholder="Discord Token"
+                value={this.props.token}
+              />
+            </div>
+          </Field>
+          <button className="button is-fullwidth is-info" onClick={this.loginDiscord}>Login</button>
+        </Modal>
       </div>
     );
+    /* eslint-enable jsx-a11y/label-has-for */
   }
 }
 
